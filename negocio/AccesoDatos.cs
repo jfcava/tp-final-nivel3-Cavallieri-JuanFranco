d87ae0689b64a3176110107c155f2cdaf2232bf2
juancava <juancavallieri@gmail.com>
2024-01-02 16:06:29 -0300

    Correccion URl acceso a datos en Web.config

--- negocio/AccesoDatos.cs
+++ negocio/AccesoDatos.cs
@@ -21,7 +21,8 @@ namespace negocio
 
         public AccesoDatos()
         {            
-            conexion = new SqlConnection("server=.\\SQLEXPRESS;database=CATALOGO_WEB_DB;integrated security=true");
+            conexion = new SqlConnection(ConfigurationManager.AppSettings["cadenaConexion"]);
+            //conexion = new SqlConnection("server=.\\SQLEXPRESS;database=CATALOGO_WEB_DB;integrated security=true");
             comando = new SqlCommand();
         }
 
